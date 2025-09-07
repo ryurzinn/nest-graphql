a44f391782da0bff4e96947394a44da84ad37183
ryurzinn <chinwenwencha47@gmail.com>
2025-09-07 15:10:21 -0300

    conexion con la db, primera consulta a la base de datos y creacion de la tabla

--- src/posts/post.entity.ts
+++ src/posts/post.entity.ts
@@ -1,14 +1,19 @@
 import {ObjectType, Field, Int} from '@nestjs/graphql';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
+@Entity()
 @ObjectType()
 export class Post {
 
+    @PrimaryGeneratedColumn()
     @Field((type) => Int)
     id: number;
 
+    @Column({type: 'text'})
     @Field()
     title: string;
 
+    @Column({type: 'text', nullable: true})
     @Field({ nullable: true })
     content?: string;
 
