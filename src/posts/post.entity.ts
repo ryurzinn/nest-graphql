9d6f9e2bc39f2f8672ca9364bc7c72efc78185a6
ryurzinn <chinwenwencha47@gmail.com>
2025-09-07 14:47:08 -0300

    primer query realizado con graphl y schema generado automaticamente por codigo

--- src/posts/post.entity.ts
+++ src/posts/post.entity.ts
@@ -1,2 +1,15 @@
+import {ObjectType, Field, Int} from '@nestjs/graphql';
 
+@ObjectType()
+export class Post {
 
+    @Field((type) => Int)
+    id: number;
+
+    @Field()
+    title: string;
+
+    @Field({ nullable: true })
+    content?: string;
+
+}
